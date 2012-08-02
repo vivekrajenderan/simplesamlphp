682f77872e44f545b8c9333ba9ebc4aa1f7cee23
olavmrk <olavmrk@44740490-163a-0410-bde0-09ae8108e29a>
2012-08-02 06:28:37 +0000

    simplesamlphp-1.9.1

--- www/shib13/sp/initSSO.php
+++ www/shib13/sp/initSSO.php
@@ -84,7 +84,7 @@ if (!isset($session) || !$session->isValid('shib13') ) {
 } else {
 
 	
-	$relaystate = $_GET['RelayState'];
+	$relaystate = $session->getRelayState();
 	
 	if (isset($relaystate) && !empty($relaystate)) {
 		SimpleSAML_Logger::info('Shib1.3 - SP.initSSO: Already Authenticated, Go back to RelayState');
